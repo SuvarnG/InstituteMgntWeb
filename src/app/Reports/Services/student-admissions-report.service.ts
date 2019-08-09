bddc0bca686d8ffa161edd11ea333721c3307f33
Manisha Manekar <Manisha.Realizertech.com>
2019-08-09 13:46:13 +0530

    Atul - changes for report cleanup

--- src/app/Reports/Services/student-admissions-report.service.ts
+++ src/app/Reports/Services/student-admissions-report.service.ts
@@ -26,10 +26,6 @@ export class StudentAdmissionsReportService {
     return httpOptions
   }
 
-  pullStudentAdmissionReport(body) {
-    return this.httpClient.post<StudentReport[]>(environment.APIBASEURL + 'Student/GetStudentAdmissionReport', body, this.getAuthHeader());
-  }
-
   public exportAsExcelFile(json: any[], excelFileName: string): void {
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
@@ -37,9 +33,13 @@ export class StudentAdmissionsReportService {
     this.saveAsExcelFile(excelBuffer, excelFileName);
   }
 
+  pullStudentAdmissionReport(body) {
+    return this.httpClient.post<StudentReport[]>(environment.APIBASEURL + 'Student/GetStudentAdmissionReport', body, this.getAuthHeader());
+  }
+
   private saveAsExcelFile(buffer: any, fileName: string): void {
     const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
     FileSaver.saveAs(data, fileName + '_Report_' + new Date().getTime() + EXCEL_EXTENSION);
   }
-  
+
 }
