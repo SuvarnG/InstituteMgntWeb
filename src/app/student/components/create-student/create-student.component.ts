b231b88ef059485583bcb44d470d196509c61535
Manisha Manekar <Manisha.Realizertech.com>
2019-08-09 17:08:32 +0530

    Atul - changes for create student code cleanup

--- src/app/student/components/create-student/create-student.component.ts
+++ src/app/student/components/create-student/create-student.component.ts
@@ -2,14 +2,10 @@ import { CourseFees, Users } from 'shared/Model/Students';
 import { CoursetypeService } from '../../../Courses/Services/coursetype.service';
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AbstractControl } from '@angular/forms'
 import { CreateNewStudentService } from '../../services/create-new-student.service';
-import { Students, CreateStudent, FeesTransaction, User, CourseType, Courses, RecentStudent } from 'shared/Model/Students';
-import { jsonpCallbackContext } from '@angular/common/http/src/module';
-import { template } from '@angular/core/src/render3';
-import { formGroupNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
-import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { CreateStudent, FeesTransaction, CourseType, Courses, RecentStudent } from 'shared/Model/Students';
 import { Router } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Utils } from '../../../Core/Utils';
@@ -21,33 +17,6 @@ import { Utils } from '../../../Core/Utils';
   styleUrls: ['./create-student.component.css']
 })
 export class CreateStudentComponent implements OnInit {
-  modalRef: BsModalRef;
-  registerForm: FormGroup;
-  submitted = false;
-  submitStudentFees = false;
-  submitStudentLogin = false;
-  showSelected: Boolean = false;
-  registerStudentCourse: FormGroup;
-  registerStudentLogin: FormGroup;
-  selectedUserValue: number;
-  selectedFeesValue: number;
-  selectedRoleValue: number;
-  selectedStudentIdValue: number;
-  selectedCourseFeesValue: number;
-  selectedStudentFirstName: string;
-  selectedStudentLastName: string;
-  selectedStudentEmail: string;
-  CalculatedDiscountedAmount: number;
-  selectedPayingFeesNow: boolean;
-  NewStudentid: number;
-  newRecentStudent: RecentStudent[];
-  fileToUpload: File = null;
-  courseFees: CourseFees[];
-  getRecentStudent: RecentStudent[];
-  users: Users[];
-  courses:Courses[];
-  courseTypeList:CourseType[];
-  thumbnailDocUrl:any;
 
   constructor(private modalService: BsModalService, private formBuilder: FormBuilder,
     public CreateNewStudentService: CreateNewStudentService,
@@ -56,9 +25,32 @@ export class CreateStudentComponent implements OnInit {
     private coursetypeService: CoursetypeService) { }
 
 
+  modalRef: BsModalRef;
+  createNewStudentForm: FormGroup;
+  submitted = false;
+  submitStudentFees = false;
+  submitStudentLogin = false;
+  showSelected: Boolean = false;
+  registerStudentCourse: FormGroup;
+  registerStudentLogin: FormGroup;
+  selectedUserValue: number;
+  selectedFeesValue: number;
+  selectedCourseFeesValue: number;
+  CalculatedDiscountedAmount: number;
+  NewStudentid: number;
+  newRecentStudent: RecentStudent[];
+  fileToUpload: File = null;
+  courseFees: CourseFees[];
+  getRecentStudent: RecentStudent[];
+  users: Users[];
+  courses: Courses[];
+  courseTypeList: CourseType[];
+  thumbnailDocUrl: any;
+
+
   ngOnInit() {
 
-    this.registerForm = this.formBuilder.group({
+    this.createNewStudentForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       middleName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -77,63 +69,48 @@ export class CreateStudentComponent implements OnInit {
       EmergencyContactNo: ['', [Validators.required, this.phoneNumberValidator]],
       Email: ['', [Validators.required, Validators.email]],
       gender: ['', Validators.required],
-      //payingFeesNow: [],
       Document: [],
       Photo: ['../../assets/images/MProfile.jpg'],
       NewImage: ['']
     });
 
-
-
-
     this.registerStudentCourse = this.formBuilder.group({
-      //StudentID: ['', Validators.required],
       CourseType: ['', Validators.required],
       CourseName: ['', Validators.required],
       CourseFees: ['', Validators.required],
       FeesAmount: ['', Validators.required],
       AnyDiscount: ['', Validators.required],
       NewDiscountedAmount: ['', Validators.required],
-      //DateofPayment: ['', Validators.required],
-      //FeesTakenBy: ['', Validators.required],
       Remark: ['', Validators.required]
     },
     );
-
-
-    this.registerStudentLogin = this.formBuilder.group({
-      FirstName: ['', Validators.required],
-      Lastname: ['', Validators.required],
-      EMailId: ['', Validators.required],
-      Role: ['', Validators.required],
-      Password: ['', Validators.required],
-      VerifyPassword: ['', Validators.required],
-      CreatedBy: ['', Validators.required]
-    });
 
   }
 
   selectAddress(event: any) {
     if (event.currentTarget.checked == true) {
-      this.registerForm.controls.Address1.setValue(this.registerForm.controls.PAddress1.value)
-      this.registerForm.controls.city.setValue(this.registerForm.controls.Pcity.value)
-      this.registerForm.controls.state.setValue(this.registerForm.controls.Pstate.value)
-      this.registerForm.controls.zipCode.setValue(this.registerForm.controls.PzipCode.value)
-
+      this.createNewStudentForm.controls.Address1.setValue(this.createNewStudentForm.controls.PAddress1.value)
+      this.createNewStudentForm.controls.city.setValue(this.createNewStudentForm.controls.Pcity.value)
+      this.createNewStudentForm.controls.state.setValue(this.createNewStudentForm.controls.Pstate.value)
+      this.createNewStudentForm.controls.zipCode.setValue(this.createNewStudentForm.controls.PzipCode.value)
     }
     else {
-      this.registerForm.controls.Address1.reset();
-      this.registerForm.controls.city.reset();
-      this.registerForm.controls.state.reset();
-      this.registerForm.controls.zipCode.reset();
+      this.createNewStudentForm.controls.Address1.reset();
+      this.createNewStudentForm.controls.city.reset();
+      this.createNewStudentForm.controls.state.reset();
+      this.createNewStudentForm.controls.zipCode.reset();
     }
   }
 
+  public user = Utils.GetCurrentUser();
+
+  get f() { return this.createNewStudentForm.controls; }
+
+  get g() { return this.registerStudentCourse.controls; }
 
   getToday(): string {
     return new Date().toISOString().split('T')[0]
   }
-
 
   matchValidator(group: FormGroup) {
     let password = this.registerStudentLogin.controls.Password.value;
@@ -142,107 +119,68 @@ export class CreateStudentComponent implements OnInit {
     if (password == verifyPassword) {
       return null;
     }
-
     return {
       mismatch: true
     }
   }
 
 
-  openModal(template: TemplateRef<any>) {
-    
+  createNewStudent(template: TemplateRef<any>) {
     this.submitted = true;
-    if (this.registerForm.invalid) {
+    if (this.createNewStudentForm.invalid) {
       return
     }
-
     //Create Student Function
     let body: CreateStudent = {
-      Gender: this.registerForm.controls.gender.value,
-      FirstName: this.registerForm.controls.firstName.value,
-      MiddleName: this.registerForm.controls.middleName.value,
-      LastName: this.registerForm.controls.lastName.value,
-      Address1: this.registerForm.controls.Address1.value,
-      City: this.registerForm.controls.city.value,
-      State: this.registerForm.controls.state.value,
-      STDCode: this.registerForm.controls.zipCode.value,
-      DOB: this.registerForm.controls.dateOfBirth.value,
-      BloodGroup: this.registerForm.controls.bloodgroup.value,
-      ContactNo: this.registerForm.controls.ContactNo.value,
-      EmergencyNo: this.registerForm.controls.EmergencyContactNo.value,
-      EmailId: this.registerForm.controls.Email.value,
-      PAddress1: this.registerForm.controls.PAddress1.value,
-      PCity: this.registerForm.controls.Pcity.value,
-      PState: this.registerForm.controls.Pstate.value,
-      PSTDCode: this.registerForm.controls.PzipCode.value,
+      Gender: this.createNewStudentForm.controls.gender.value,
+      FirstName: this.createNewStudentForm.controls.firstName.value,
+      MiddleName: this.createNewStudentForm.controls.middleName.value,
+      LastName: this.createNewStudentForm.controls.lastName.value,
+      Address1: this.createNewStudentForm.controls.Address1.value,
+      City: this.createNewStudentForm.controls.city.value,
+      State: this.createNewStudentForm.controls.state.value,
+      STDCode: this.createNewStudentForm.controls.zipCode.value,
+      DOB: this.createNewStudentForm.controls.dateOfBirth.value,
+      BloodGroup: this.createNewStudentForm.controls.bloodgroup.value,
+      ContactNo: this.createNewStudentForm.controls.ContactNo.value,
+      EmergencyNo: this.createNewStudentForm.controls.EmergencyContactNo.value,
+      EmailId: this.createNewStudentForm.controls.Email.value,
+      PAddress1: this.createNewStudentForm.controls.PAddress1.value,
+      PCity: this.createNewStudentForm.controls.Pcity.value,
+      PState: this.createNewStudentForm.controls.Pstate.value,
+      PSTDCode: this.createNewStudentForm.controls.PzipCode.value,
       Photo: this.CreateNewStudentService.thumbnailUrl,
-      IsDocumentSubmitted: this.registerForm.controls.IsDocumentSubmitted.value,
-      Document:this.thumbnailDocUrl,
-      //PayingFees: this.registerForm.controls.payingFeesNow.value
+      IsDocumentSubmitted: this.createNewStudentForm.controls.IsDocumentSubmitted.value,
+      Document: this.thumbnailDocUrl,
     };
-
-
-    this.CreateNewStudentService.createNewStudent(body).subscribe(data =>
+    this.CreateNewStudentService.createNewStudent(body).subscribe(data =>{}
       // do something, if upload success
-      this.CreateNewStudentService.getRecentlyCreatedStudent().subscribe(res => {
-        this.getRecentStudent = res
-      })
+      // this.CreateNewStudentService.getRecentlyCreatedStudent().subscribe(res => {
+      //   this.getRecentStudent = res
+      // })
     );
 
     //Open Popup function
-      this.modalRef = this.modalService.show(template, {
-        animated: true,
-        backdrop: 'static',
-        class:'modal-xl'
-      });
-      this.coursetypeService.courseTypeList().subscribe(res=>{
-        this.courseTypeList=res
-      });
-      // this.CreateNewStudentService.GetCourseNameFromCourseType(this.selectedUserValue);
-      this.CreateNewStudentService.getUsersListForFeesTaken().subscribe(res => {
-        this.users = res
-      });
-    
+    this.modalRef = this.modalService.show(template, {
+      animated: true,
+      backdrop: 'static',
+      class: 'modal-xl'
+    });
+    this.coursetypeService.courseTypeList().subscribe(res => {
+      this.courseTypeList = res
+    });
+    // this.CreateNewStudentService.getUsersListForFeesTaken().subscribe(res => {
+    //   this.users = res
+    // });
+
   }
 
   onImageSelected(event: any) {
     if (event.target.files.length) {
       const file = event.target.files[0];
-      this.registerForm.get('Photo').setValue(file);
+      this.createNewStudentForm.get('Photo').setValue(file);
     }
   }
-
-  openModal2(template: TemplateRef<any>) {
-    this.submitStudentFees = true;
-    if (this.registerStudentCourse.invalid) {
-      return
-    }
-
-    this.modalRef = this.modalService.show(template, {
-      animated: true,
-      backdrop: 'static'
-    });
-    this.CreateNewStudentService.getRolesList();
-  }
-
-
-  get f() { return this.registerForm.controls; }
-
-  get g() { return this.registerStudentCourse.controls; }
-
-  get e() { return this.registerStudentLogin.controls }
-
-
-  onSubmitStudentLogin() {
-    this.submitStudentLogin = true;
-
-    //stop here if form is invalid
-    if (this.registerStudentLogin.invalid) {
-      return;
-    }
-  }
-
-
 
   phoneNumberValidator(
     control: AbstractControl
@@ -253,174 +191,74 @@ export class CreateStudentComponent implements OnInit {
       : { invalidNumber: { valid: false, value: control.value } }
   }
 
-  showUpload() {
-    if(this.registerForm.controls.IsDocumentSubmitted.value==true)
-    this.showSelected = true;
-    else
-    this.showSelected = false;
-  }
-
-  // hideUpload() {
-  //   if(this.showSelected==true){
-  //     this.showSelected = false;
-  //   }
-    
-  // }
-
-  public user=Utils.GetCurrentUser();
-
-
   createStudentCourse(feesTransaction: FeesTransaction) {
     this.submitStudentFees = true;
     if (this.registerStudentCourse.invalid) {
       return
     }
-
     let body = {
       Id: 0,
       CourseId: this.registerStudentCourse.controls.CourseName.value,
-      //StudentId: this.selectedStudentIdValue,
       CourseFees: this.registerStudentCourse.controls.CourseFees.value,
       NewDiscountedAmount: this.registerStudentCourse.controls.NewDiscountedAmount.value,
       DateOfPayment: new Date(),
       FeesPaid: this.registerStudentCourse.controls.FeesAmount.value,
       FeesTakenBy: this.user.userId.toString(),
-      PendingFees:(Number(this.registerStudentCourse.controls.NewDiscountedAmount.value)-Number(this.registerStudentCourse.controls.FeesAmount.value)),
-      BranchId:this.user.BranchId,
-      InstituteId:this.user.InstituteId,
+      PendingFees: (Number(this.registerStudentCourse.controls.NewDiscountedAmount.value) - Number(this.registerStudentCourse.controls.FeesAmount.value)),
+      BranchId: this.user.BranchId,
+      InstituteId: this.user.InstituteId,
       CourseCompleted: false,
       Discount: this.registerStudentCourse.controls.AnyDiscount.value,
       TotalFees: this.registerStudentCourse.controls.NewDiscountedAmount.value,
       Remark: this.registerStudentCourse.controls.Remark.value,
       IsActive: true
-
     };
-
-      this.modalRef.hide();
-      this.CreateNewStudentService.createStudentCourse(body).subscribe(data => { this.router.navigateByUrl('/StudentList') });
-   
+    this.modalRef.hide();
+    this.CreateNewStudentService.createStudentCourse(body).subscribe(data => { this.router.navigateByUrl('/StudentList') });
   }
 
 
-
-  //   createStudentLogin(user: User) {
-
-  //     this.submitStudentLogin = true;
-  //     if (this.registerStudentLogin.invalid) {
-  //       return
-  //     }
-
-  //     let body: User = {
-  //       Id: 0,
-  //       //StudentId:this.registerStudentLogin.controls.StudentID.value,
-  //       FirstName: this.selectedStudentFirstName,
-  //       LastName: this.selectedStudentLastName,
-  //       Email: this.selectedStudentEmail,
-  //       Password: this.registerStudentLogin.controls.VerifyPassword.value,
-  //       RoleId: this.selectedRoleValue,
-  //       CreatedBy: this.registerStudentLogin.controls.CreatedBy.value
-
-  //     };
-
-  //     if(this.registerStudentLogin.controls.Password.value!=this.registerStudentLogin.controls.VerifyPassword.value)
-  //     {
-  //       alert("Password did not matched..Please verify password")
-  //     }
-  //     else
-  //     {
-  //     if (confirm("Are you sure to create a login?")) {
-  //       this.CreateNewStudentService.createStudentLogin(body).subscribe((data) => 
-  //         {this.modalRef.hide(),
-  //         this.router.navigateByUrl('/StudentList') });
-
-  //     }
-  //   }
+  // getCourseNameFromCourseType(id:number){
+  //   this.CreateNewStudentService.getCourseNameFromCourseType(id).subscribe(data=>{
+  //     this.listCourses = data
+  //   })
   // }
-
-
-
-  // handleFileInput(event: any) {
-  //   if (event.target.files.length) {
-  //     const file = event.target.files[0];
-  //     this.registerForm.get('Document').setValue(file);
-  //   }
-  // }
-
 
   handleFileInput(event: any) {
     if (event.target.files.length) {
       const file = event.target.files[0];
-      if(file.name.includes(".txt") || file.name.includes(".pdf"))
-      {
-        this.registerForm.get('Document').setValue(file);
+      if (file.name.includes(".txt") || file.name.includes(".pdf")) {
+        this.createNewStudentForm.get('Document').setValue(file);
       }
-      else{
-            alert("Please select Proper file");
+      else {
+        alert("Please select Proper file");
       }
-      
     }
   }
 
   handlePhotoInput(event: any) {
     if (event.target.files.length) {
       const file = event.target.files[0];
-      this.registerForm.get('Photo').setValue(file);
+      this.createNewStudentForm.get('Photo').setValue(file);
     }
   }
 
-  // onUploadFile() {
-  //   const formData = new FormData();
-  //   formData.append('profile', this.registerForm.get('Document').value)//this.registerForm.get('Documents').value);
-  //   this.CreateNewStudentService.postFile(formData).subscribe(res => {
-  //     console.log(res);
-  //   });
-  // }
-
   onUploadFile() {
     const formData = new FormData();
-    formData.append('File', this.registerForm.get('Document').value);
+    formData.append('File', this.createNewStudentForm.get('Document').value);
     this.CreateNewStudentService.postFile(formData).subscribe(
-      res=>{ 
-             
-          if(res['type']==4){ 
-           this.thumbnailDocUrl='Http://'+ res['body']['Message'];
-           
-          }
-                        
+      res => {
+        if (res['type'] == 4) {
+          this.thumbnailDocUrl = 'Http://' + res['body']['Message'];
+        }
       }
     );
   }
 
-  // onUploadPhoto()
-  // {
-  //   const formData = new FormData();
-  //   formData.append('profile',this.registerForm.get('Photo').value)//this.registerForm.get('Documents').value);
-  //   this.CreateNewStudentService.postPhoto(formData).subscribe(res=>{
-  //         console.log(res);        
-  //   });
-  // }
-
-
   onUploadPhoto() {
     const formData = new FormData();
-    formData.append('profile', this.registerForm.get('Photo').value)//this.registerForm.get('Documents').value);
+    formData.append('profile', this.createNewStudentForm.get('Photo').value)
     this.CreateNewStudentService.postPhoto(formData)
-  }
-
-  selectStudentId(event) {
-    this.selectedStudentIdValue = event.target.value;
-  }
-
-  selectStudentFirstName(event) {
-    this.selectedStudentFirstName = event.target.value;
-  }
-
-  selectStudentLastName(event) {
-    this.selectedStudentLastName = event.target.value;
-  }
-
-  selectStudentEmail(event) {
-    this.selectedStudentEmail = event.target.value;
   }
 
   selectCourseFees(event) {
@@ -435,34 +273,21 @@ export class CreateStudentComponent implements OnInit {
     this.selectedFeesValue = event.target.value;
   }
 
-  selectRole(event) {
-    this.selectedRoleValue = event.target.value;
-  }
-
-
-  selectPayingFeesNow(event) {
-    this.selectedPayingFeesNow = event.target.value;
-  }
-
   getCourseNameFromCourseType(courses: Courses) {
-    this.CreateNewStudentService.getCourseNameFromCourseType(this.selectedUserValue).subscribe(res=>{
-      this.courses=res
+    this.CreateNewStudentService.getCourseNameFromCourseType(this.selectedUserValue).subscribe(res => {
+      this.courses = res
     })
-
   }
 
   getCourseFeesFromCourseName(courses: Courses) {
     this.CreateNewStudentService.getCourseFeesFromCourseName(this.selectedFeesValue).subscribe(res => {
       this.courseFees = res
     })
-
   }
 
   calculateDiscountedAmount() {
-    
     this.CalculatedDiscountedAmount = ((this.registerStudentCourse.controls.CourseFees.value) - ((this.registerStudentCourse.controls.AnyDiscount.value / 100) * this.registerStudentCourse.controls.CourseFees.value))
     this.registerStudentCourse.controls.NewDiscountedAmount.setValue(this.CalculatedDiscountedAmount)
-
   }
 
 }
--- src/app/student/services/create-new-student.service.ts
+++ src/app/student/services/create-new-student.service.ts
@@ -1,86 +1,43 @@
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from "rxjs/operators";
 import { environment } from '../../../environments/environment';
-import { CreateStudent, FeesTransaction,User,CourseType, Courses,Users,Roles,RecentStudent, CourseFees,ThumbnailUrl } from 'shared/Model/Students';
+import { CreateStudent, FeesTransaction, CourseType, Courses, RecentStudent, CourseFees } from 'shared/Model/Students';
 import { Utils } from '../../Core/Utils';
-
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateNewStudentService {
   public listCourseType: CourseType[];
-  public listCourses:Courses[];
-  public listCourseFees:CourseFees[];
-  public listUsers:Users[];
-  public listRoles:Roles[];
+  public listCourses: Courses[];
+  public listCourseFees: CourseFees[];
   public listRecentStudent: RecentStudent[]
-  public NewStudId:unknown;
-  public StudentId:number;
-  public thumbnailUrl:any='../../assets/images/MProfile.jpg';
+  public NewStudId: unknown;
+  public StudentId: number;
+  public thumbnailUrl: any = '../../assets/images/MProfile.jpg';
 
   private Url = environment.APIBASEURL + 'Student/CreateStudent';
-  private FeesUrl=environment.APIBASEURL+'Student/SaveOrUpdateFeesTransactionForStudent';
-  private StudentLoginUrl=environment.APIBASEURL+'Login/CreateNewUser';
-  private GetAllCourseTypeUrl=environment.APIBASEURL+'Course/GetAllCourseType';
-  private uploadUrl=environment.APIBASEURL+'Upload/PostUserImage'
+  private FeesUrl = environment.APIBASEURL + 'Student/SaveOrUpdateFeesTransactionForStudent';
+  private uploadUrl = environment.APIBASEURL + 'Upload/PostUserImage'
 
   constructor(private http: HttpClient) { }
 
-  getAuthHeader(){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${Utils.GetAccessToken()}`
-      })      
-    };
-    return httpOptions
+  createNewStudent(students: CreateStudent) {
+    return this.http.post<CreateStudent>(this.Url, students, Utils.getAuthHeader())
   }
 
-  createNewStudent(students:CreateStudent){
-    return this.http.post<CreateStudent>(this.Url,students,this.getAuthHeader()) 
+  createStudentCourse(feesTransaction) {
+    return this.http.post<FeesTransaction>(this.FeesUrl, feesTransaction, Utils.getAuthHeader())
   }
 
-  createStudentCourse(feesTransaction){
-    return this.http.post<FeesTransaction>(this.FeesUrl,feesTransaction,this.getAuthHeader())
+  getCourseNameFromCourseType(id) {
+    return this.http.get(environment.APIBASEURL + 'Student/GetCourseNameFromCourseType' + '/' + id, Utils.getAuthHeader()).pipe(map(data => data as Courses[]))
   }
 
-  createStudentLogin(user:User){
-    return this.http.post<User>(this.StudentLoginUrl,user,this.getAuthHeader())
+  getCourseFeesFromCourseName(id) {
+    return this.http.get(environment.APIBASEURL + 'Student/GetCourseFeesFromCourseName' + '/' + id, Utils.getAuthHeader()).pipe(map(data => data as CourseFees[]))
   }
-
-  getRecentlyCreatedStudent(){
-    return this.http.get(environment.APIBASEURL+'Student/GetRecentlyCreatedStudent',this.getAuthHeader()).pipe(map(data => data as RecentStudent[]))
-  }
-
-
-  getCourseNameFromCourseType(id){  
-    return this.http.get(environment.APIBASEURL+'Student/GetCourseNameFromCourseType'+'/'+id,this.getAuthHeader()).pipe(map(data => data as Courses[]))
-  }
-
-  getCourseFeesFromCourseName(id){
-    return this.http.get(environment.APIBASEURL+'Student/GetCourseFeesFromCourseName'+'/'+id,this.getAuthHeader()).pipe(map(data => data as CourseFees[]))
-  }
-
-  getUsersListForFeesTaken(){
-    return this.http.get(environment.APIBASEURL+'Student/GetUsersListForFeesTaken',this.getAuthHeader()).pipe(map(data => data as Users[]))
- 
-  }
-
-  getRolesList(){
-    this.http.get(environment.APIBASEURL+'Student/GetRolesList').toPromise().then(result=>this.listRoles=result as Roles[]) 
-  }
-
-
-// postFile(formData)
-//   {
-//     return this.http.post<any>(environment.APIBASEURL+'Upload/PostFile', formData, {
-//       reportProgress: true,
-//       observe: 'events'
-//     });
-//   }
 
   postFile(formData) {
     return this.http.post<any>(environment.APIBASEURL + 'Upload/PostFile', formData, {
@@ -89,35 +46,15 @@ export class CreateNewStudentService {
     });
   }
 
-
-  // postPhoto(formData)
-  // {
-  //   return this.http.post<any>(this.uploadUrl, formData, {
-  //     reportProgress: true,
-  //     observe: 'events'
-  //   });
-  // }
-
-  // postPhoto(formData)
-  // {
-  //   return this.http.post<any>(this.uploadUrl, formData, {
-  //     reportProgress: true,
-  //     observe: 'events'
-  //   }).toPromise().then(result=>this.thumbnailUrl=result).catch();
-  // }
-
-  postPhoto(formData)
-  {
+  postPhoto(formData) {
     return this.http.post<any>(this.uploadUrl, formData, {
       reportProgress: true,
       observe: 'events'
     }).subscribe(
-      res=>{      
-          if(res['type']==4){
-           this.thumbnailUrl='Http://'+ res['body']['Message'];
-           
-          }
-                        
+      res => {
+        if (res['type'] == 4) {
+          this.thumbnailUrl = 'Http://' + res['body']['Message'];
+        }
       }
     )
   }
