8435dca2d34e4fd6151804a1e61e0f2f91aee88f
Manisha Manekar <Manisha.Realizertech.com>
2019-08-09 17:32:43 +0530

    Atul - code cleanup for student list

--- src/app/Student/Components/student-list/student-list.component.ts
+++ src/app/Student/Components/student-list/student-list.component.ts
@@ -3,10 +3,10 @@ import { CourseType, Courses, FeesTransactions } from 'shared/Model/Students';
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { StudentslistService } from '../../Services/studentslist.service';
-import { BsModalService, BsModalRef, ModalContainerComponent, ModalOptions } from 'ngx-bootstrap/modal';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { BsModalService, BsModalRef, ModalOptions } from 'ngx-bootstrap/modal';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { formatDate } from '@angular/common';
-import { Students, UpdateStudent } from 'shared/Model/Students'
+import { Students } from 'shared/Model/Students'
 import { Utils } from '../../../Core/Utils';
 import { CoursetypeService } from '../../../Courses/Services/coursetype.service';
 
@@ -18,6 +18,15 @@ import { CoursetypeService } from '../../../Courses/Services/coursetype.service'
 })
 export class StudentListComponent implements OnInit {
 
+  constructor(private modalService: BsModalService,
+    private router: Router,
+    private StudentslistService: StudentslistService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private createNewStudentService: CreateNewStudentService,
+    private coursetypeService: CoursetypeService) { }
+
+  public thumbnailUrl: any = '../../assets/images/MProfile.jpg';
   modalRef: BsModalRef
   payStudent: Students;
   feesTransaction: FeesTransactions[];
@@ -32,27 +41,19 @@ export class StudentListComponent implements OnInit {
   students: Students[];
   CourseTypeList: CourseType[];
   courseNameList: Courses[];
-  public thumbnailUrl: any = '../../assets/images/MProfile.jpg';
   urlDocument: any;
   studentDetails: Students;
-  getPendingFees:number;
-
-  constructor(private modalService: BsModalService,
-    private router: Router,
-    private StudentslistService: StudentslistService,
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private createNewStudentService: CreateNewStudentService,
-    private coursetypeService: CoursetypeService) { }
+  getPendingFees: number;
 
   ngOnInit() {
     this.returnUrl = '/create-student';
+
     this.getAllStudents(this.user.InstituteId, this.user.BranchId);
 
     this.StudentFeesForm = this.formBuilder.group({
       FeesAmount: ['', Validators.required],
-
     })
+
     this.registerUpdateStudent = this.formBuilder.group({
       Gender: ['', Validators.required],
       FirstName: ['', Validators.required],
@@ -75,21 +76,23 @@ export class StudentListComponent implements OnInit {
       ZipCode: ['', Validators.required],
       IsDocumentSubmitted: [],
       Photo: [],
-
       //Document:[]
     })
+
   }
+
+  public user = Utils.GetCurrentUser();
+
+  get f() { return this.registerUpdateStudent.controls };
+  get fc() { return this.StudentFeesForm.controls };
 
   getAllStudents(InstituteId: number, BranchId: number) {
     InstituteId: this.user.InstituteId;
     BranchId: this.user.BranchId;
     this.StudentslistService.getAllStudents(InstituteId, BranchId).subscribe(res => {
       this.students = res
-      //console.log("Students Data:" + this.students);
     });
   }
-
-  public user = Utils.GetCurrentUser();
 
   deleteStudent(id: number) {
     if (confirm("Are you sure to delete this student? ")) {
@@ -104,7 +107,7 @@ export class StudentListComponent implements OnInit {
       this.CourseTypeList = res
     });
     this.studentID = s.StudentId,
-      this.thumbnailUrl = s.Photo
+    this.thumbnailUrl = s.Photo
     this.registerUpdateStudent.patchValue({
       StudentId: s.StudentId,
       Gender: s.Gender,
@@ -136,18 +139,18 @@ export class StudentListComponent implements OnInit {
 
     this.modalRef = this.modalService.show(editStudent, { class: 'modal-xl' })
   }
+
   selectCourseType(event) {
-    //this.selectedCourseTypeValue = event.target.value;
     this.createNewStudentService.getCourseNameFromCourseType(event.target.value).subscribe(res => {
       this.courseNameList = res
     });
   }
+
   onSubmitEditStudent() {
     this.submitted = true;
     if (this.registerUpdateStudent.invalid) {
       return
     }
-
     this.submitted = false;
     let body = {
       StudentId: this.studentID,
@@ -174,80 +177,35 @@ export class StudentListComponent implements OnInit {
       Photo: this.thumbnailUrl
     }
 
-    this.StudentslistService.editStudent(body).subscribe(data => { this.getAllStudents(this.user.InstituteId, this.user.BranchId), this.modalRef.hide() })
-
-
+    this.StudentslistService.editStudent(body).subscribe(data => {
+      this.getAllStudents(this.user.InstituteId, this.user.BranchId),
+        this.modalRef.hide()
+    })
   }
 
   openStudentDetailsPopup(studentDetails: TemplateRef<any>, selectedStudent) {
     this.studentID = selectedStudent.StudentId;
-    debugger;
-      this.urlDocument = selectedStudent.Document;
-      this.studentDetails = selectedStudent;
-      this.StudentslistService.getFeesTransactionDetails(this.studentID).subscribe(res=>{
-        this.feesTransaction = res;
-      })
-      
-    // this.registerUpdateStudent.patchValue(
-    //   {
-    //     StudentId: s.StudentId,
-    //     Gender: s.Gender,
-    //     FirstName: s.FirstName,
-    //     MiddleName: s.MiddleName,
-    //     LastName: s.LastName,
-    //     Address1: s.Address1,
-    //     CourseType: s.CourseType,
-    //     Course: s.Course,
-    //     City: s.City,
-    //     State: s.State,
-    //     ZipCode: s.STDCode,
-    //     DOB: formatDate(s.DOB, 'yyyy-MM-dd', 'en'),
-    //     PAddress1: s.PAddress1,
-    //     PCity: s.PCity,
-    //     PState: s.PState,
-    //     PSTDCode: s.PSTDCode,
-    //     BloodGroup: s.BloodGroup,
-    //     Photo: s.Photo,
-    //     ContactNo: s.ContactNo,
-    //     EmergencyNo: s.EmergencyNo,
-    //     Email: s.EmailId,
-    //     IsDocumentSubmitted: s.IsDocumentSubmitted,
-    //     Document: s.Document,
-
-    //   })
-
+    this.urlDocument = selectedStudent.Document;
+    this.studentDetails = selectedStudent;
+    this.StudentslistService.getFeesTransactionDetails(this.studentID).subscribe(res => {
+      this.feesTransaction = res;
+    })
     this.modalRef = this.modalService.show(studentDetails, { class: 'modal-xl' })
   }
-  ShowFeesTransactionTemplate(StudentFees: TemplateRef<any>, _payStudent: Students) {
-    debugger;
+
+  showFeesTransactionTemplate(StudentFees: TemplateRef<any>, _payStudent: Students) {
     this.StudentFeesForm.reset();
-    // this.getFeesTransactionDetails(_payStudent.StudentId);
     this.StudentslistService.getFeesTransactionDetails(_payStudent.StudentId).subscribe(res => {
       this.feesTransaction = res;
       let totalPaid = 0;
       this.feesTransaction.forEach(x => totalPaid += x.FeesPaid);
       _payStudent.PendingFees = _payStudent.TotalFees - totalPaid;
-      this.getPendingFees=_payStudent.TotalFees - totalPaid;
+      this.getPendingFees = _payStudent.TotalFees - totalPaid;
     })
     this.modalRef = this.modalService.show(StudentFees, { class: 'modal-xl' })
-
     this.payStudent = _payStudent;
     console.log(this.payStudent);
   }
-  showUpload() {
-    this.showSelected = true;
-  }
-
-  hideUpload() {
-    this.showSelected = false;
-  }
-
-  printPage() {
-    window.print();
-  }
-
-  get f() { return this.registerUpdateStudent.controls }
-  get fc() { return this.StudentFeesForm.controls }
 
   onImageSelected(event: any) {
     if (event.target.files.length) {
@@ -266,9 +224,7 @@ export class StudentListComponent implements OnInit {
         }
       }
     );
-
   }
-
 
   onSubmitStudentFees(student) {
     debugger;
@@ -278,18 +234,13 @@ export class StudentListComponent implements OnInit {
       DateOfPayment: new Date(),
       FeesPaid: this.StudentFeesForm.controls.FeesAmount.value,
       FeesTakenBy: this.user.userId,
-      PendingFees:this.getPendingFees-this.StudentFeesForm.controls.FeesAmount.value
+      PendingFees: this.getPendingFees - this.StudentFeesForm.controls.FeesAmount.value
     }
-    this.StudentslistService.onSubmitStudentFees(body).subscribe(data =>{
-       this.payStudent;
-       if(data!=null)
-       this.getAllStudents(this.user.InstituteId,this.user.BranchId);
+    this.StudentslistService.onSubmitStudentFees(body).subscribe(data => {
+      this.payStudent;
+      if (data != null)
+        this.getAllStudents(this.user.InstituteId, this.user.BranchId);
     })
-    // this.StudentslistService.onSubmitStudentFees(this.studentID).subscribe(res=>{
-    //   this.feesTransaction=res
-    // })
   }
-
-
 
 }
--- src/app/Student/Services/studentslist.service.ts
+++ src/app/Student/Services/studentslist.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { Students, UpdateStudent, StudentPendingFeesList,FeesTransactions } from 'shared/Model/Students';
+import { Students, UpdateStudent, StudentPendingFeesList, FeesTransactions } from 'shared/Model/Students';
 import { Utils } from '../../Core/Utils';
 import { map } from "rxjs/operators";
 
@@ -17,29 +17,17 @@ export class StudentslistService {
 
   constructor(private http: HttpClient) { }
 
-
-  getAuthHeader(){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${Utils.GetAccessToken()}`
-      })      
-    };
-    return httpOptions
-  }
-
- 
   getAllStudents(InstituteId: number, BranchId: number) {
     debugger;
-    return this.http.get<Students[]>(this.Url + '/' + InstituteId + '/' + BranchId, this.getAuthHeader()).pipe(map(data => data as Students[]))
+    return this.http.get<Students[]>(this.Url + '/' + InstituteId + '/' + BranchId, Utils.getAuthHeader()).pipe(map(data => data as Students[]))
   }
 
   deleteStudent(id: number) {
-    return this.http.post<void>(this.DeleteStdnt + "/" + id, null, this.getAuthHeader())
+    return this.http.post<void>(this.DeleteStdnt + "/" + id, null, Utils.getAuthHeader())
   }
 
   editStudent(student) {
-    return this.http.post<UpdateStudent>(this.EditStnt, student, this.getAuthHeader())
+    return this.http.post<UpdateStudent>(this.EditStnt, student, Utils.getAuthHeader())
   }
 
   postPhoto(formData) {
@@ -50,15 +38,16 @@ export class StudentslistService {
   }
 
   getAllStudentsPendingFeesDetails(BranchId: number) {
-    return this.http.post<StudentPendingFeesList[]>(environment.APIBASEURL + 'Chart/GetNotificationData' +'/' + BranchId,null, this.getAuthHeader()).pipe(map(data => data as StudentPendingFeesList[]))
+    return this.http.post<StudentPendingFeesList[]>(environment.APIBASEURL + 'Chart/GetNotificationData' + '/' + BranchId, null, Utils.getAuthHeader()).pipe(map(data => data as StudentPendingFeesList[]))
   }
-  getFeesTransactionDetails(id:number)
-  {
+
+  getFeesTransactionDetails(id: number) {
     debugger;
-    return this.http.get(environment.APIBASEURL+'Student/GetAllFeesTransaction'+'/'+id,this.getAuthHeader()).pipe(map(data => data as FeesTransactions[]))
+    return this.http.get(environment.APIBASEURL + 'Student/GetAllFeesTransaction' + '/' + id, Utils.getAuthHeader()).pipe(map(data => data as FeesTransactions[]))
   }
-  onSubmitStudentFees(feesTransactions)
-  {
-    return this.http.post<FeesTransactions[]>(environment.APIBASEURL+'Bank/SaveFeesTransaction',feesTransactions,this.getAuthHeader())
+
+  onSubmitStudentFees(feesTransactions) {
+    return this.http.post<FeesTransactions[]>(environment.APIBASEURL + 'Bank/SaveFeesTransaction', feesTransactions, Utils.getAuthHeader())
   }
+
 }
